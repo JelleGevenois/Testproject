abe7a19c312289252623f529aafae491e0e72495
Aime <Aime@192.168.1.3>
2013-03-13 21:24:04 +0100

    nieuw versie

--- src/testproject/Testproject.java
+++ src/testproject/Testproject.java
@@ -14,6 +14,9 @@ public class Testproject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       //gewijzigd code
+    }
+    public void extramethode(){
+    
     }
 }
